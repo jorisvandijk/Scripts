bc30b91823d2c5466fc21f83176b887a19ded012
Joris <joris@jorisvandijk.com>
2020-08-23 19:02:29 +0200

    Expanded options of script.

--- git.py
+++ git.py
@@ -3,7 +3,7 @@
 
 import os
 
-repo = input("Which repo do you want to update?\n(1) Python\n(2) Obsidian\n(3) Dotfiles\n:")
+repo = input("Which repo do you want to update?\n(1) Python\n(2) Obsidian\n(3) Scripts\n(4) Dotfiles\n:")
 
 if repo == "1":
     folder = "/home/joris/GitLab/python/"
@@ -17,6 +17,12 @@ elif repo == "2":
     add = "add ."
     origin = "origin"
 
+elif repo == "3":
+    folder = "/home/joris/Scripts/"
+    git = "git"
+    add = "add ."
+    origin = "origin"
+
 else:
     folder = "/home/joris/"
     git = "/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
@@ -25,9 +31,23 @@ else:
       
 os.chdir(f"{folder}")
 os.system(f"{git} status")
+changes = input("\nPush all changes? (y/n) or (q) to quit\n:")
 
-commit = input("\nPlease add a commit message\n:")
-os.system(f"{git} {add} && {git} commit -m \"{commit}\"")
+if changes == "y":
+    commit = input("\nPlease add a commit message\n:")
+    os.system(f"{git} {add} && {git} commit -m \"{commit}\"")
+    os.system(f"{git} status")
+
+elif changes == "q":
+    quit()
+
+else:
+    change = input("\nWhich file would you like to push?\n:")
+    os.system(f"{git} add {change}")
+    commit = input("\nPlease add a commit message\n:")
+    os.system(f"{git} commit -m \"{commit}\"")
+    os.system(f"{git} status")
+
 push = input("\nAre you sure you wish to push the changes? (y/n)\n:")
     
 if push == "y":
