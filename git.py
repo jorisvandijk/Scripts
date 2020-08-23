8adc7deaa479407461a5cb1c1fab0493f873efb9
Joris <joris@jorisvandijk.com>
2020-08-23 21:24:09 +0200

    Adding more functions to git.py.

--- git.py
+++ git.py
@@ -7,71 +7,105 @@ class bcolors:
     green = '\033[92m'
     ENDC = '\033[0m'
 
+question = input("\nWhat would you like to do?\n(1) Check status of all repositories\n(2) Push repositories\n(3) Pull repositories\n:")\
 
-print(f"Warning: No active frommets remain. Continue?")
-repo = input("Which repository do you want to update?\n(1) Python\n(2) Obsidian\n(3) Scripts\n(4) Dotfiles\n(c) Check all repositories for changes.\n:")
+if question == "1":
 
-if repo == "1":
-    folder = "/home/joris/GitLab/python/"
-    git = "git"
-    add = "add ."
-    origin = "origin"
-
-elif repo == "2":
-    folder = "/home/joris/Notes/joris/"
-    git = "git"
-    add = "add ."
-    origin = "origin"
-
-elif repo == "3":
-    folder = "/home/joris/Scripts/"
-    git = "git"
-    add = "add ."
-    origin = "origin"
-
-elif repo == "4":
-    folder = "/home/joris/"
-    git = "/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
-    add = "add -u"
-    origin = ""
-
-elif repo == "c":
-    print(f"{bcolors.green}Python:{bcolors.ENDC}")
+    print(f"{bcolors.green}\nPython:{bcolors.ENDC}")
     os.system("cd ~/GitLab/python/ && git status")
-    print(f"\n{bcolors.green}Obsidian:{bcolors.ENDC}")
+    print(f"\n{bcolors.green}\nObsidian:{bcolors.ENDC}")
     os.system("cd ~/Notes/joris/ && git status")
-    print(f"\n{bcolors.green}Scripts:{bcolors.ENDC}")
+    print(f"\n{bcolors.green}\nScripts:{bcolors.ENDC}")
     os.system("cd ~/Scripts/ && git status")
-    print(f"\n{bcolors.green}Dotfiles:{bcolors.ENDC}")
+    print(f"\n{bcolors.green}\nDotfiles:{bcolors.ENDC}")
     os.system("cd ~/ && /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME status")
     quit()
 
-else:
-    print("That is not a repository.")
-    quit()
-      
-os.chdir(f"{folder}")
-os.system(f"{git} status")
-changes = input("\nPush all changes? (y/n) or (q) to quit.\n:")
+elif question == "2":
+    repo = input("\nWhich repository do you want to update?\n(1) Python\n(2) Obsidian\n(3) Scripts\n(4) Dotfiles\n:")
 
-if changes == "y":
-    commit = input("\nPlease add a commit message.\n:")
-    os.system(f"{git} {add} && {git} commit -m \"{commit}\"")
+    if repo == "1":
+        folder = "/home/joris/GitLab/python/"
+        git = "git"
+        add = "add ."
+        origin = "origin"
+
+    elif repo == "2":
+        folder = "/home/joris/Notes/joris/"
+        git = "git"
+        add = "add ."
+        origin = "origin"
+
+    elif repo == "3":
+        folder = "/home/joris/Scripts/"
+        git = "git"
+        add = "add ."
+        origin = "origin"
+
+    elif repo == "4":
+        folder = "/home/joris/"
+        git = "/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
+        add = "add -u"
+        origin = ""
+
+    else:
+        print("That is not a repository.")
+        quit()
+        
+    os.chdir(f"{folder}")
     os.system(f"{git} status")
+    changes = input("\nPush all changes? (y/n) or (q) to quit.\n:")
 
-elif changes == "q":
-    quit()
+    if changes == "y":
+        commit = input("\nPlease add a commit message.\n:")
+        os.system(f"{git} {add} && {git} commit -m \"{commit}\"")
+        os.system(f"{git} status")
 
+    elif changes == "q":
+        quit()
+
+    else:
+        change = input("\nWhich file would you like to push? Or press (q) to quit.\n:")
+        if change == "q":
+            quit()
+        else:
+            os.system(f"{git} add {change}")
+            commit = input("\nPlease add a commit message\n:")
+            os.system(f"{git} commit -m \"{commit}\"")
+            os.system(f"{git} status")
+
+    push = input("\nAre you sure you wish to push the changes? (y/n)\n:")
+        
+    if push == "y":
+        os.system(f"{git} push {origin}")
+    else:
+        quit()
+
+elif question == "3":
+    repo = input("\nWhich repository do you want to pull from?\n(1) Python\n(2) Obsidian\n(3) Scripts\n(4) Dotfiles\n:")
+
+    if repo == "1":
+        os.chdir(f"/home/joris/GitLab/python/")
+        os.system(f"git pull")
+        quit()
+
+    if repo == "2":
+        os.chdir(f"/home/joris/Notes/joris/")
+        os.system(f"git pull")
+        quit()
+
+    elif repo == "3":
+        os.chdir(f"/home/joris/Scripts/")
+        os.system(f"git pull")
+
+    elif repo == "4":
+        os.chdir(f"/home/joris/")
+        os.system(f"/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME pull")
+
+    else:
+        print("That is not a repository.")
+        quit()
+        
 else:
-    change = input("\nWhich file would you like to push?\n:")
-    os.system(f"{git} add {change}")
-    commit = input("\nPlease add a commit message\n:")
-    os.system(f"{git} commit -m \"{commit}\"")
-    os.system(f"{git} status")
-
-push = input("\nAre you sure you wish to push the changes? (y/n)\n:")
-    
-if push == "y":
-    os.system(f"{git} push {origin}")
-else:
+    print("Wrong input.")
     quit()
