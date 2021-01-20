7d0597e8f2c85c3d33076b5761c0ad1c8a2ea61a
Joris van Dijk <joris@jorisvandijk.com>
2021-01-20 12:01:25 +0100

    Some sorting

---
+++ bash/install_system.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Jorisvandijk.com - Post install script. based on a script by HexDSL.
+
+me="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+u="$USER"
+
+menu()
+	{
+		echo "usage:   " $me "[OPTION]"
+		echo " "
+		echo "init:    Do something"
+		echo " "
+
+	}
+
+init()
+	{
+		echo "make something of this"
+	}
+
+if [ -n "$1" ]; then
+  $1
+else
+  menu
+fi
