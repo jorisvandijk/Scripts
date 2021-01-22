a2114d2dd9861ed7640375a3bc0385f630d2bcc3
Joris van Dijk <joris@jorisvandijk.com>
2021-01-22 19:32:34 +0100

    Adding new script

---
+++ bash/test_account.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# Usage: To either add or remove a (test) user for 
+#        running tests within a VM.
+#
+# Script by Joris van Dijk | gitlab.com/jorisvandijk 
+#
+#          Published under GPL-3.0-or-later
+
+echo -n "Would you like to [c]reate a user or [d]elete a user?: "
+read make
+if [ $make == c ]
+then
+    echo -n "Enter the username and press [ENTER]: "
+    read name
+    sudo useradd -m $name
+    sudo passwd $name
+    sudo usermod -aG wheel $name
+elif [ $make == d ]
+then
+    echo -n "Enter the username and press [ENTER]: "
+    read name
+    sudo userdel -fr $name
+else 
+    echo "Wrong input!"
+fi
