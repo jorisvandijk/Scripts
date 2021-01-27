3ccfc9d278eaa5735053a43c7210e94c543cc6e3
Joris van Dijk <joris@jorisvandijk.com>
2021-01-27 04:53:17 +0100

    Awesome random wallpaper script

--- bash/wallpaper.sh
+++ bash/wallpaper.sh
@@ -1,86 +1,15 @@
-#!/bin/bash
 #
-# Usage: To create random wallpaper made with PotD 
-#        from Wikipedia and a random English word
+# Usage: Unsplash wallpaper changer
 #
-# Script by HexDSL | gitlab.com/hexdsl 
-# Alterations by Joris van Dijk | gitlab.com/jorisvandijk
+# Dotfile by Joris van Dijk | gitlab.com/jorisvandijk 
 #
-#          Published under GPL-3.0-or-later
+#          Published under GPL-3.0-or-later             
+              
+KEYWORDS="nature, backgrounds, hd"                  
 
-beginnytime="1099267200"
-nowtime=$(date +%s)
-count=0
-basedir="/home/joris/Pictures/Wallpapers"
-currun=0
-numruns=1
-imagename="image"
-
-function getpic {
-	randate=$(shuf -i $beginnytime-$nowtime -n 1)
-	getDate=$(date -d @$randate +%Y-%m-%d)
-	baseURL="https://en.wikipedia.org/wiki/Template:POTD/"$getDate
-	#echo $baseURL;
-	picURL=$(curl -s $baseURL | grep -o -E -m1 'href="([^"#]+).jpg|jpeg"' | cut -d'"' -f2)
-	procpic;
-}
-function procpic {
-	#echo https://en.wikipedia.org$picURL;
-	pt=$(echo $picURL | sed  's/\/wiki\/File\://g' 2>/dev/null)
-	echo $pt
-	imgURL=$(curl -s https://en.wikipedia.org$picURL | sed  's/>/\n/g'i 2>/dev/null | grep -o -E -m1 'href="//upload([^"#]+)'$pt'"' | cut -d'"' -f2)
-	if [ -z "$imgURL" ]
-	then
-		((count++))
-		#echo $count;
-		if [ "$count" -ge 10 ]
-		then
-			echo "failed 10 times"
-			exit
-		else
-			getpic
-		fi
-	else
-		((currun++))
-		echo $currun
-		name="$basedir/$imagename$currun.jpg";
-			curl -o $name https:$imgURL && convert -resize 2590x1090^ -gravity Center -crop 2590x1090+0+0 -quality 80 -delete 1--1 $name $name &&
-            echo "<script>window.location=\"$baseURL\"</script>" > "$basedir/wiki$currun.html" &&
-				#	echo "<script>window.location=\"$baseURL\"</script>" > "$basedir/wiki.html" &&
-			font=$(convert -list font | grep "Font: " | sed -e '/Noto-/d' -e '/D050000L/d' -e '/JoyPixels/d' | shuf | head -n1 | sed -e 's/Font: //' -e 's/ //g')
-			echo $font
-			word=$(aspell dump master | sed "/'s/d" | shuf | head -n1)
-            convert $name -fill "#000000"  -font $font -pointsize 300 \
-            -gravity Center -annotate +10-10 "$word" \
-			$name
-
-			ffmpeg -i "$name" -vf hue=h=310:s=3.8:b=3,eq=contrast=4 "$name" -y >> /dev/null 2>&1 &&
-
-			convert "$name"  \
-			-set option:distort:viewport '%wx%h+0+0' \
-			-colorspace CMYK -separate null: \
-			\( -size 3x3 xc: \( +clone -negate \) \
-			+append \( +clone -negate \) -append \) \
-			-virtual-pixel tile -filter gaussian \
-			\( +clone -distort SRT 60 \) +swap \
-			\( +clone -distort SRT 30 \) +swap \
-			\( +clone -distort SRT 45 \) +swap \
-			\( +clone -fill black -colorize 100 -distort SRT 0 \)  +swap +delete \
-			-compose Overlay -layers composite \
-			-set colorspace CMYK -combine -colorspace RGB \
-			"$name" &&
-			convert "$name"  \
-			-virtual-pixel edge -channel M -fx "p[-10,10]" \
-			"$name"
-			convert "$name" -crop 2580x1080+10-10 "$name"
-
-			if [ $currun -ge $numruns ];then
-				feh --bg-fill "$basedir/image1.jpg"
-				exit;
-			else
-				getpic
-			fi
-	fi
-}
-
-getpic;
+case $1 in
+  new)
+    wget https://source.unsplash.com/random/1920x1080/?$KEYWORDS -4 -O $HOME/Pictures/wallpaper.jpg
+    feh --bg-fill $HOME/Pictures/wallpaper.jpg
+    ;;
+esac
