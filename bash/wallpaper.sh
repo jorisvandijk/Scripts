1c0b1bb575aacc2ada85a8e39ee76b63a27392af
Joris <joris@jorisvandijk.com>
2021-02-02 23:15:10 +0100

    Some explaination

--- bash/wallpaper.sh
+++ bash/wallpaper.sh
@@ -4,7 +4,13 @@
 #
 # Dotfile by Joris van Dijk | gitlab.com/jorisvandijk 
 #
-#          Published under GPL-3.0-or-later             
+#          Published under GPL-3.0-or-later           
+
+# Add "bindsym $mod+a $e ~/Scripts/bash/wallpaper.sh new" to i3 config
+# to enable changing the random Unspash wallpaper.
+
+# Add "exec_always --no-startup-id ~/Scripts/bash/wallpaper.sh boot" to
+# the i3 config to enable random Unsplah wallpaper at boot.
               
 KEYWORDS="painting, art"                  
 
