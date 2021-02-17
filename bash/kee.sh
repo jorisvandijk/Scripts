a7f82e331e199dfbd5e6d1980eed7bf865b281b0
Joris <joris@jorisvandijk.com>
2021-02-17 21:50:18 +0100

    Adding

---
+++ bash/kee.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# Usage: Simplyfy FvdM update MyPolo.nl
+#
+# Script by Joris van Dijk | gitlab.com/jorisvandijk 
+#
+#          Published under GPL-3.0-or-later
+
+cd $HOME/Documents/Kee/ || return
+git pull  
+keepassxc &
+disown &&
+exit
+
+# Closing the terminal does not work yet!
+# Have to search for a way to do this...
