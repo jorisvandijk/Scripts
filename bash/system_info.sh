c8b1c8c0651e65443726b32dbc15edbb2bdbc57b
Joris <joris@jorisvandijk.com>
2021-02-25 15:41:15 +0100

    Added blob

--- bash/system_info.sh
+++ bash/system_info.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+#
+# Usage: Notify-send current system info.
+#
+# Script by Joris van Dijk | gitlab.com/jorisvandijk 
+#
+#          Published under GPL-3.0-or-later
+
 notify-send -u critical -t 10000 "$(
     date;
     echo;
