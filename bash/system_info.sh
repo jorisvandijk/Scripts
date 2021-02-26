3585bbe1f0526de4ce92378950921cae685636f2
Joris <joris@jorisvandijk.com>
2021-02-27 00:40:43 +0100

    Adding repository check

--- bash/system_info.sh
+++ bash/system_info.sh
@@ -7,6 +7,38 @@
 #
 #          Published under GPL-3.0-or-later
 
+cd $HOME/Scripts/
+if [[ $(git status --porcelain) ]]; then
+    one="Scripts "
+fi
+
+cd $HOME/Dotfiles/
+if [[ $(git status --porcelain) ]]; then
+    two="Dotfiles "
+fi
+
+cd $HOME/Documents/Kee/
+if [[ $(git status --porcelain) ]]; then
+    three="Kee "
+fi
+
+cd $HOME/.config/FreeTube/
+if [[ $(git status --porcelain) ]]; then
+    four="FreeTube "
+fi
+
+cd $HOME/Pictures/wallpapers/
+if [[ $(git status --porcelain) ]]; then
+     five="Wallpapers "
+fi
+
+cd $HOME/Documents/Notes/
+if [[ $(git status --porcelain) ]]; then
+    six="Notes "
+fi
+
+repo=$one$two$three$four$five$six
+
 notify-send -u critical -t 10000 "$(
     date;
     echo;
@@ -23,4 +55,10 @@ notify-send -u critical -t 10000 "$(
     echo
     echo "Number of updates:"
     checkupdates | wc -l
-    )" 
+    echo
+    if [[ $repo ]]; then
+    echo -e "There are updates in:\n$repo"
+    else 
+    echo "All repositories are up to date!"
+    fi
+)" 
