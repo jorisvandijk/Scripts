3e159dc54a84a611678164ffa499e11e6e70fc6d
Joris <joris@jorisvandijk.com>
2021-02-26 01:03:44 +0100

    Adding comment

--- bash/system_info.sh
+++ bash/system_info.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #
-# Usage: Notify-send current system info.
+# Usage: Notify-send current system info, called from the
+#        i3 config file by bindsym.
 #
 # Script by Joris van Dijk | gitlab.com/jorisvandijk 
 #
