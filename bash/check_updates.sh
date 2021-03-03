3e80597566f243eddf8ed99454951c5a48dc56fe
Joris <joris@jorisvandijk.com>
2021-03-03 01:42:41 +0100

    Check for updates script

---
+++ bash/check_updates.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# Usage: Notify-send current system info, called from the
+#        i3 config file by bindsym.
+#
+# Script by Joris van Dijk | gitlab.com/jorisvandijk 
+#
+#          Published under GPL-3.0-or-later
+
+notify-send -u critical -t 15000 "$(checkupdates)"
