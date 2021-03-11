abb9d019a9db373503f4206d19dfed822dbbc643
Bob <bob@burgers.com>
2021-03-12 00:05:05 +0100

    test

--- test-1.sh
+++ test-1.sh
@@ -1,6 +1,9 @@
 # Set user and home
 user=$SUDO_USER
-home="/home/$user" 
+home="/home/$user"
 GN="test"
-sudo su
-sudo -u $user ssh-keygen -t rsa -q -f "$home/.ssh/id_rsa" -N "" -C "$GN" && cat $home/.ssh/id_rsa.pub | xclip -sel clip
+
+echo $user
+echo $home
+sudo -u $user ssh-keygen -t rsa -q -f "$home/.ssh/id_rsa" -N "" -C "$GN"
+cat $home/.ssh/id_rsa.pub | xclip -sel clip
