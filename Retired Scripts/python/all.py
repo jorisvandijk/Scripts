35839b03746fe9de5bd813d965f8b127c0d628c9
Joris <joris@jorisvandijk.com>
2021-03-15 18:49:30 +0100

    Lots

--- Retired Scripts/python/all.py
+++
@@ -1,7 +0,0 @@
-import os
-
-print("The following files are available to execute:\n")
-os.system("cd ~/Scripts/ && ls -1a *.py")
-file = input("\nWhich script would you like to execute?\n(You only need to type the name, not the extention)\n:")
-os.system("clear")
-os.system(f"python -u ~/Scripts/{file}.py")
